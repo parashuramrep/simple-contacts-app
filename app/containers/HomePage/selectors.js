ecf21013262b13b3ecf4560e2ee46ae632c16994
Václav Zeman <vaclav.zeman@strv.com>
2017-02-04 16:21:07 +0100

    Remove redundant folders

--- app/containers/HomePage/selectors.js
+++ app/containers/HomePage/selectors.js
@@ -15,12 +15,12 @@ export const filteredClientsSelector = createSelector(
     const clients = state.get('data');
 
     if (clients) {
-      const search = state.get('searchQuery').toLowerCase();
+      const search = state.get('searchQuery').toLowerCase().trim();
       // Loop through array and the 1st level of objects - like general, job etc.
       return clients.filter((item) => item.keySeq().some((subItem) =>
         // Loop through subObjects like obj["general"] and then through obj["general"]["phone"] and
         // return true if there's a match with the search query
-        item.get(subItem).keySeq().some((value) => item.getIn([subItem, value]).toLowerCase().includes(search))
+        item.get(subItem).keySeq().some((value) => item.getIn([subItem, value]).toLowerCase().trim().includes(search))
       ));
     }
 
